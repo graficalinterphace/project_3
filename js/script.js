c624302c8b64a36f603813d212d21a8db025f34c
graficalinterphace <122955638+graficalinterphace@users.noreply.github.com>
2023-05-02 23:04:35 -0500

    Set up slides

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//map
 let map;
 
 function initMap(){
@@ -6,14 +7,12 @@ function initMap(){
     center: location,
     zoom: 11,
   });
-  
   const icon = {
     url: "docs/assets/dumpling.png",
     scaledSize: new google.maps.Size(50, 50),
     origin: new google.maps.Point(0,0),
     anchor: new google.maps.Point(0, 0)
   };
-  
   var marker = new google.maps.Marker({
     position: location,
     map: map,
@@ -21,25 +20,44 @@ function initMap(){
     animation: google.maps.Animation.DROP,
     icon: icon
   });
-
+  
+  //animations
   marker.addListener("mouseover", () => {
     marker.setAnimation(google.maps.Animation.BOUNCE);
     setTimeout(function(){ marker.setAnimation(null); }, 700);
   });
-
   var infoWindow = new google.maps.InfoWindow({
     position: location,
     content: marker.getTitle()
   });
-  
   marker.addListener("click", () => {
     if (infoWindow.opened){
       infoWindow.close();
       infoWindow.opened = false;
     } else {
-    infoWindow.open(marker.getMap(), marker);
+      infoWindow.open(marker.getMap(), marker);
     }
   });
 }
 
 initMap();
+
+//slides
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("norseimg");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+let slideIndex = 1;
